3641dd07e40a9f88f4ea5358b7f426ba2f880f51
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2025-05-31 04:13:40 +0530

    Edit and Update Blog Post Actions

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
 
   get "/blog_posts/new" => "blog_posts#new", as: :new_blog_post
   get "/blog_posts/:id" => "blog_posts#show", as: :blog_post
+  patch "/blog_posts/:id" => "blog_posts#update"
+  get "/blog_posts/:id/edit" => "blog_posts#edit", as: :edit_blog_post
   post "/blog_posts" => "blog_posts#create", as: :blog_posts
 
   # Defines the root path route ("/")
