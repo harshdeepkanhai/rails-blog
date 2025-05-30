1b2b5c8b53aaa12096b321326f29889270934806
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2025-05-31 02:47:58 +0530

    Add blog posts with show action and links.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  get "/blog_posts/:id" => "blog_posts#show"
+
   # Defines the root path route ("/")
   # root "posts#index"
   root "blog_posts#index"
