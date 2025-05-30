59cf032767ed99e05a7e012fa8fc882e71fd9e1a
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2025-05-31 05:09:49 +0530

    Authenticating with Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
+  # :registerable, Not able to register
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 end
